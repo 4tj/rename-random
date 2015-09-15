2e5e01b3e1433709c5864d5d2fce8cf0bff825ca
wb-zhaokuncheng <wb-zhaokuncheng@alibaba-inc.com>
2015-09-15 11:06:00 +0800

    initial files

---
+++ README.md
@@ -0,0 +1,15 @@
+random-rename
+================================
+
+Rename file by random
+
+## Install
+
+```{bash}
+npm install random-rename
+```
+## Author
+Kevin Zhao, http://qinshou.me
+
+## License
+MIT
---
+++ bin/cli.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var random = require('../lib/random');
+
+var fileName = process.argv.slice(2)[0];
+var filePath = path.join(process.cwd(), fileName);
+
+var outputName = random(16) + path.extname(fileName);
+
+fs.rename(filePath, path.join(path.dirname(filePath), outputName), function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log('Renamed complete ' + fileName + ' ==> ' + outputName);
+});
